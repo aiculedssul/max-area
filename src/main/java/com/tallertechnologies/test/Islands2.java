ce2a41ffb99aba1f6c19aed4c07304c51384d88f
aiculedssul <spintos@gmail.com>
2022-05-13 13:09:49 -0300

    adding multithreading support

--- src/main/java/com/tallertechnologies/test/Islands2.java
+++ src/main/java/com/tallertechnologies/test/Islands2.java
@@ -1,6 +1,11 @@
 package com.tallertechnologies.test;
 
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.util.stream.Collectors.toSet;
 
 public class Islands2 {
 
@@ -12,11 +17,14 @@ public class Islands2 {
      **/
 
     public static void main(String[] args) {
-        int grid1[][] = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 0
-                , 1, 1, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        int grid1[][] = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                         {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                         {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                         {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                         {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
         int grid2[][] = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1}, {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1},
                 {0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1}, {0, 1, 0, 0
                 , 1, 1, 0, 0, 0, 1, 0, 0, 1}, {0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1},
@@ -41,7 +49,7 @@ public class Islands2 {
      * @param  column vertical position in the grid array
      * @return the total amount of slots with land
      */
-    public static int checkAround(int[][] grid, int row, int column) {
+    public static int checkAround(int[][] grid, int row, int column, AreaInfo info, int border) {
         int size = 0;
 
         //validate that we do not surpass the limits of the grid
@@ -53,42 +61,145 @@ public class Islands2 {
 
                 size++;
 
+                //grid.length - 1 or 0
+                if (row == border) {
+                    info.borders.add(column);
+                }
+
                 //this is to check above
-                size += checkAround(grid, row - 1, column);
+                size += checkAround(grid, row - 1, column, info, border);
                 //this is to check right
-                size += checkAround(grid, row, column + 1);
+                size += checkAround(grid, row, column + 1, info, border);
                 //this is to check below
-                size += checkAround(grid, row + 1, column);
+                size += checkAround(grid, row + 1, column, info, border);
                 //this is to check left
-                size += checkAround(grid, row, column - 1);
+                size += checkAround(grid, row, column - 1, info, border);
             }
         }
+        info.size = size;
         return size;
     }
 
     /**
      * this method returns the maximum area of an island in grid
+     * this method needs to be enhanced, since it only uses 2 threads and does not allow configuring more,
+     * but ideally it splits the grid in 2 and calculates the area of each island.
+     * for those islands that were cut because of the split, then it merges the borders of each array
      *
      * @param  grid   a set of islands
      * @return the maximum area of an island
      */
     public static int maxAreaOfIsland(int[][] grid) {
-        Integer size = 0;
-        Integer area = 0;
 
-        // iterate over the set of islands
+        CountDownLatch latch = new CountDownLatch(2);
+
+        int[][] firstGrid = Arrays.copyOfRange(grid, 0, grid.length/2);
+        final List<AreaInfo> firstAreas = new ArrayList<>();
+        Runnable firstRun = () -> {
+            firstAreas.addAll(calculateAreas(firstGrid,firstGrid.length-1));
+            latch.countDown();
+        };
+
+        int[][] secondGrid = Arrays.copyOfRange(grid, grid.length/2, grid.length);
+        final List<AreaInfo> secondAreas = new ArrayList<>();
+        Runnable secondRun = () -> {
+            secondAreas.addAll(calculateAreas(secondGrid,0));
+            latch.countDown();
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.execute(firstRun);
+        executor.execute(secondRun);
+        executor.shutdown();
+
+        try {
+            latch.await();
+        } catch(InterruptedException ex) {
+            throw new RuntimeException(ex);
+        }
+        int size = mergeAreas(firstAreas,secondAreas);
+
+        return size;
+    }
+
+    private static List<AreaInfo> calculateAreas(int[][] grid, int border) {
+        List<AreaInfo> areas = new ArrayList<>();
         for (int x = 0; x < grid.length; x++) {
             for (int y = 0; y < grid[x].length; y++) {
                 if (grid[x][y] == 1) {
-                    size = checkAround(grid, x, y);
-                    if (size > area) {
-                        area = size;
-                    }
-                    size = 0;
+                    AreaInfo areaInfo = new AreaInfo();
+                    areaInfo.size = checkAround(grid, x, y, areaInfo, border);
+                    areas.add(areaInfo);
                 }
             }
         }
-        return area;
+        return areas;
+    }
+
+    private static class AreaInfo {
+        public Integer size;
+        public List<Integer> borders = new ArrayList<>();
+
+        public boolean areasMatch(AreaInfo area) {
+            return this.borders.stream()
+                    .anyMatch(area.borders.stream()
+                                    .collect(toSet())
+                                    ::contains);
+        }
+    }
+
+    //this method needs to be enhanced because it iterates more than needed
+    private static int mergeAreas(List<AreaInfo> firstAreas, List<AreaInfo> secondAreas) {
+        Set<AreaInfo> areaInfoSet = new HashSet<>();
+        areaInfoSet.addAll(secondAreas);
+        areaInfoSet.addAll(firstAreas);
+        Map<AreaInfo,AreaInfo> secondAreasToFinalArea = new HashMap<>();
+        Map<AreaInfo,AreaInfo> firstAreaToFinalArea = new HashMap<>();
+        int size = 0;
+        for (AreaInfo first : firstAreas) {
+            AreaInfo finalArea = null;
+            for (AreaInfo second : secondAreas) {
+                if (first.areasMatch(second)) {
+                    if (secondAreasToFinalArea.containsKey(second)) {
+                        finalArea = secondAreasToFinalArea.get(second);
+                        finalArea.size += first.size;
+                        finalArea.borders.addAll(first.borders);
+                    }
+                    if (firstAreaToFinalArea.containsKey(first)) {
+                        finalArea = firstAreaToFinalArea.get(first);
+                        finalArea.size += second.size;
+                        finalArea.borders.addAll(second.borders);
+                    }
+                    if (finalArea == null) {
+                        finalArea = new AreaInfo();
+                        finalArea.size = first.size + second.size;
+                        finalArea.borders.addAll(second.borders);
+                    }
+                    areaInfoSet.remove(second);
+                    areaInfoSet.remove(first);
+                    areaInfoSet.add(finalArea);
+                    secondAreasToFinalArea.put(second,finalArea);
+                    firstAreaToFinalArea.put(first,finalArea);
+                }
+                if (second.size > size) {
+                    size = second.size;
+                }
+                if (finalArea != null && finalArea.size > size) {
+                    size = finalArea.size;
+                }
+            }
+            if (first.size > size) {
+                size = first.size;
+            }
+        }
+        if (firstAreas.isEmpty()) {
+            for (AreaInfo second : secondAreas) {
+                if (second.size > size) {
+                    size = second.size;
+                }
+            }
+        }
+        return size;
     }
 
 }
